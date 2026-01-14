65ed686cae55d58dd0c74fcaa2d2f9d80949fc6f
matolagit <mateuslesilva12@gmail.com>
2026-01-14 18:45:49 -0300

    .

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -16,6 +16,4 @@ public class ContaTerminal {
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
 
     }
-
-
 }
